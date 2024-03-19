6a2e6a532b996f0f05a1d0dfe30a2aa1c5a8c3a4
AnasGamal <agamal@stetson.edu>
2024-03-19 18:14:47 -0400

    improve UI and layout

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,26 +2,48 @@ package com.example.tipcalculator
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Button
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.slider.Slider
+
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val calculateButton: Button = findViewById(R.id.calculateButton)
+//        val calculateButton: Button = findViewById(R.id.calculateButton)
         val billAmountEditText: EditText = findViewById(R.id.billAmountEditText)
-        val tipPercentageEditText: EditText = findViewById(R.id.tipPercentageEditText)
+        val tipPercentageSlider: Slider = findViewById(R.id.tipPercentageSlider)
         val tipResultTextView: TextView = findViewById(R.id.tipResultTextView)
 
-        calculateButton.setOnClickListener {
-            val billAmount = billAmountEditText.text.toString().toDoubleOrNull() ?: 0.0
-            val tipPercentage = tipPercentageEditText.text.toString().toDoubleOrNull() ?: 0.0
-            val tipAmount = calculateTip(billAmount, tipPercentage)
-            tipResultTextView.text = "Tip Amount: $${"%.2f".format(tipAmount)}"
+        tipPercentageSlider.setLabelFormatter { value ->
+            "$value%"
         }
+
+        billAmountEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                updateTipAmount(billAmountEditText.text.toString(), tipPercentageSlider.value, tipResultTextView)
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        tipPercentageSlider.addOnChangeListener { _, value, _ ->
+            updateTipAmount(billAmountEditText.text.toString(), value, tipResultTextView)
+        }
+
+        }
+
+    private fun updateTipAmount(billAmountText: String, tipPercentage: Float, tipResultTextView: TextView) {
+        val billAmount = billAmountText.toDoubleOrNull() ?: 0.0
+        val tipAmount = calculateTip(billAmount, tipPercentage.toDouble())
+        tipResultTextView.text = "Tip Amount: ${"%.2f".format(tipAmount)}"
     }
 
     private fun calculateTip(billAmount: Double, tipPercentage: Double): Double {
